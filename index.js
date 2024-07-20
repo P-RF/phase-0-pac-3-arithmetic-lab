75b486358677565ff0bafa20fa4b8764257db642
Priscila Rodriguez Fresco <priscila.rodriguezfresco@gmail.com>
2024-07-19 23:30:44 -0400

    compleete lab

---
+++ index.js
@@ -0,0 +1,49 @@
+function add () {
+}
+
+function subtract () {
+}
+
+function multiply () {
+}
+
+function divide () {
+}
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+let number = n;
+
+function increment(n) {
+    return (n + 1);
+}
+
+//reset number
+number = n;
+
+function decrement(n) {
+    return (n - 1);
+}
+
+function makeInt(n) {
+    parseInt("n", 10);
+    return parseInt(n, 10);
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
